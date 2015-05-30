7278ea4133adc7dfddda1197b6dd4f1d7f56d7ef
Bartlomiej Wolk <bartlomiejwolk@gmail.com>
2015-05-30 11:12:30 +0200

    Create template classes

--- AudioPlayer.cs
+++ AudioPlayer.cs
@@ -1,15 +1,89 @@
 ﻿using UnityEngine;
-using System.Collections;
 
-public class AudioPlayer : MonoBehaviour {
+namespace AudioPlayerEx {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    public sealed class AudioPlayer : MonoBehaviour {
+
+        #region CONSTANTS
+
+        public const string Version = "v0.1.0";
+        public const string Extension = "AudioPlayer";
+
+        #endregion CONSTANTS
+
+        #region DELEGATES
+        #endregion DELEGATES
+
+        #region EVENTS
+        #endregion EVENTS
+
+        #region FIELDS
+
+#pragma warning disable 0414
+        /// <summary>
+        ///     Allows identify component in the scene file when reading it with
+        ///     text editor.
+        /// </summary>
+        [SerializeField]
+        private string componentName = "AudioPlayer";
+#pragma warning restore0414
+
+        #endregion FIELDS
+
+        #region INSPECTOR FIELDS
+
+        [SerializeField]
+        private string description = "Description";
+
+        #endregion INSPECTOR FIELDS
+
+        #region PROPERTIES
+
+        /// <summary>
+        ///     Optional text to describe purpose of this instance of the component.
+        /// </summary>
+        public string Description {
+            get { return description; }
+            set { description = value; }
+        }
+
+        #endregion PROPERTIES
+
+        #region UNITY MESSAGES
+
+        private void Awake() { }
+
+        private void FixedUpdate() { }
+
+        private void LateUpdate() { }
+
+        private void OnEnable() { }
+
+        private void Reset() { }
+
+        private void Start() { }
+
+        private void Update() { }
+
+        private void OnValidate() { }
+
+        private void OnCollisionEnter(Collision collision) { }
+
+        private void OnCollisionStay(Collision collision) { }
+
+        private void OnCollisionExit(Collision collision) { }
+
+        #endregion UNITY MESSAGES
+
+        #region EVENT INVOCATORS
+        #endregion INVOCATORS
+
+        #region EVENT HANDLERS
+        #endregion EVENT HANDLERS
+
+        #region METHODS
+        #endregion METHODS
+
+    }
+
 }
--- Editor/AudioPlayerEditor.cs
+++ Editor/AudioPlayerEditor.cs
@@ -1,15 +1,70 @@
-﻿using UnityEngine;
-using System.Collections;
+﻿using UnityEditor;
 
-public class AudioPlayerEditor : MonoBehaviour {
+namespace AudioPlayerEx {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    [CustomEditor(typeof(AudioPlayer))]
+    [CanEditMultipleObjects]
+    public sealed class AudioPlayerEditor : Editor {
+        #region FIELDS
+
+        private AudioPlayer Script { get; set; }
+
+        #endregion FIELDS
+
+        #region SERIALIZED PROPERTIES
+
+        private SerializedProperty description;
+
+        #endregion SERIALIZED PROPERTIES
+
+        #region UNITY MESSAGES
+
+        public override void OnInspectorGUI() {
+            serializedObject.Update();
+
+            DrawVersionLabel();
+            DrawDescriptionTextArea();
+
+            EditorGUILayout.Space();
+
+            serializedObject.ApplyModifiedProperties();
+        }
+
+        private void OnEnable() {
+            Script = (AudioPlayer)target;
+
+            description = serializedObject.FindProperty("description");
+        }
+
+        #endregion UNITY MESSAGES
+
+        #region INSPECTOR CONTROLS
+
+        private void DrawVersionLabel() {
+            EditorGUILayout.LabelField(
+                string.Format(
+                    "{0} ({1})",
+                    AudioPlayer.Version,
+                    AudioPlayer.Extension));
+        }
+
+        private void DrawDescriptionTextArea() {
+            description.stringValue = EditorGUILayout.TextArea(
+                description.stringValue);
+        }
+
+        #endregion INSPECTOR
+
+        #region METHODS
+
+        [MenuItem("Component/AudioPlayer")]
+        private static void AddEntryToComponentMenu() {
+            if (Selection.activeGameObject != null) {
+                Selection.activeGameObject.AddComponent(typeof(AudioPlayer));
+            }
+        }
+
+        #endregion METHODS
+    }
+
 }
